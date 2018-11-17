269f53c4c3e45c43315867856863119e802644f7
GlyphGryph <glyphgryph@gmail.com>
2018-11-16 23:05:25 -0500

    Fully implemented genericize method
    All tests added and passing
    Created create_generics_list function in response to a bug to make testable

--- medication_genericizer.rb
+++ medication_genericizer.rb
@@ -1,16 +1,40 @@
 class MedicationGenericizer
+  def self.create_generics_list(medications)
+    medications.inject({}) do |result, medication|
+      if(medication[:generic])
+        result[medication[:rxcui]] = medication
+      end
+      result
+    end
+  end
+
   # Input prescriptions are compared against input medications
   # Returns prescription/medication_ids of prescriptions which use a non-generic
   # medication for which a generic alternative exists
   def self.genericize(prescriptions, medications)
-    updates = []
-    prescriptions.each do |prescription|
-      updates.push(
-        { prescription_id: prescription[:id],
-          medication_id: prescription[:medication_id]
-        }
-      )
+    meds_by_id = medications.inject({}) do |result, medication|
+      result[medication[:id]] = medication
+      result
     end
+
+    generics_by_rxcui = create_generics_list(medications)
+
+    updates = prescriptions.inject([]) do |result, prescription|
+      current_medication = meds_by_id[prescription[:medication_id]]
+      unless current_medication
+        raise "Medication invalid for prescription #{prescription[:id]}"
+      end
+      generic = generics_by_rxcui[current_medication[:rxcui]]
+      if(generic && generic[:id] != prescription[:medication_id])
+        result.push(
+          { prescription_id: prescription[:id],
+            medication_id: generic[:id]
+          }
+        )
+      end
+      result
+    end
+
     return {
       prescription_updates: updates
     }
--- test_medication_genericizer.rb
+++ test_medication_genericizer.rb
@@ -3,9 +3,9 @@ require "./medication_genericizer"
 
 class TestMedicationGenericizer < Minitest::Test
   def setup
-    medications = [
+    @medications = [
       {
-        "id": "nb1",
+        "id": "g1",
         "ndc": "ecuzioqsigu",
         "rxcui": "1",
         "description": "Acetaminophen 297 mg",
@@ -15,7 +15,7 @@ class TestMedicationGenericizer < Minitest::Test
         "updated_at": "2015-10-25T13:47:28.572Z"
       },
       {
-        "id": "g1",
+        "id": "nb1",
         "ndc": "ufietuinycf",
         "rxcui": "1",
         "description": "Tylenol 297 mg",
@@ -35,22 +35,22 @@ class TestMedicationGenericizer < Minitest::Test
         "updated_at": "2015-10-25T13:48:40.516Z"
       }
     ]
-    prescriptions = [
+    @prescriptions = [
       { # Replaced
         "id": "p1",
         "medication_id": "nb1",
         "created_at": "2015-10-25T13:49:43.728Z",
         "updated_at": "2015-10-25T13:49:43.728Z"
-      #}, { # Not replaced, already generic
-      #  "id": "p2-",
-      #  "medication_id": "g1",
-      #  "created_at": "2015-10-25T13:50:21.550Z",
-      #  "updated_at": "2015-10-25T13:50:21.550Z"
-      #}, { # Not replaced, no generic available
-      #  "id": "p3",
-      #  "medication_id": "nb2",
-      #  "created_at": "2015-10-25T13:50:21.550Z",
-      #  "updated_at": "2015-10-25T13:50:21.550Z"
+      }, { # Not replaced, already generic
+        "id": "p2",
+        "medication_id": "g1",
+        "created_at": "2015-10-25T13:50:21.550Z",
+        "updated_at": "2015-10-25T13:50:21.550Z"
+      }, { # Not replaced, no generic available
+        "id": "p3",
+        "medication_id": "nb2",
+        "created_at": "2015-10-25T13:50:21.550Z",
+        "updated_at": "2015-10-25T13:50:21.550Z"
       },
       { # Replaced
         "id": "p4",
@@ -59,22 +59,29 @@ class TestMedicationGenericizer < Minitest::Test
         "updated_at": "2015-10-25T13:49:43.728Z"
       },
     ]
-    @expected_output = {
-      "prescription_updates": [
-        {
-          "prescription_id": "p1",
-          "medication_id": "nb1"
-        },
-        {
-          "prescription_id": "p4",
-          "medication_id": "nb1"
-        }
-      ]
-    }
-    @result = MedicationGenericizer.genericize(prescriptions, medications)
+  end
+
+  def test_generics_list
+    result = MedicationGenericizer.create_generics_list(@medications)
+    assert_equal 1, result.keys.length
+    assert_equal '1', result.keys.first
+    assert_equal 'g1', result['1'][:id]
   end
 
   def test_has_correct_ouput
-    assert_equal @expected_output, @result
+    result = MedicationGenericizer.genericize(@prescriptions, @medications)
+    expected_output = {
+      "prescription_updates": [
+        {
+          "prescription_id": "p1",
+          "medication_id": "g1"
+        },
+        {
+          "prescription_id": "p4",
+          "medication_id": "g1"
+        }
+      ]
+    }
+    assert_equal expected_output, result
   end
 end
